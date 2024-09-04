2333b207b6b0463c6bd0657188efdf2e7f6ed2eb
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-09-04 17:18:36 -0400

    Changed these to useSelector and useDispatch from Redux

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
+import { useSelector, useDispatch } from "react-redux";
 import Cart from '../components/Cart';
 import { useStoreContext } from '../utils/GlobalState';
 import {
@@ -15,7 +15,10 @@ import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector(state => state)
+
+  const dispatch = useDispatch()
+
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
